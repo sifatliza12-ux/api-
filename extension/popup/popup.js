5b3ed0563ee8a0cb9fb70974e7a2c590c8dd1ea1
Sifat Jahan <sifatliza12@gmail.com>
2026-06-30 23:36:38 +0600

    redesign login ui

--- extension/popup/popup.js
+++ extension/popup/popup.js
@@ -1,6 +1,6 @@
 /**
  * popup.js
- * Handles the popup UI interactions for the API Builder extension.
+ * Handles the premium popup interactions for Craft Api.
  */
 
 function initPopup() {
@@ -9,17 +9,17 @@ function initPopup() {
 
     if (loginButton) {
         loginButton.addEventListener('click', () => {
-            // TODO: Add login logic here.
+            // TODO: Implement login flow here.
         });
     }
 
     if (trialButton) {
         trialButton.addEventListener('click', () => {
-            // TODO: Add free trial flow here.
+            // TODO: Implement free trial flow here.
         });
     }
 
-    // TODO: Add any future popup initialization logic here.
+    // TODO: Add future popup initialization logic here.
 }
 
 // Initialize on popup load.
